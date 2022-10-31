9635fd6e6c4bbcf960cc63cc32def730a817c37e
Vladimir Filimonov <69769504+PepegaClasus@users.noreply.github.com>
2022-10-31 06:15:36 -0700

    Update dateFormat.groovy

--- vars/dateFormat.groovy
+++ vars/dateFormat.groovy
@@ -7,6 +7,6 @@ def DateFormat (){
         SimpleDateFormat formatter = new SimpleDateFormat("HH:mm:ss")
         Date date = new Date()
         String newDate = formatter.format(date)
-        echo newDate
+        println newDate
 
 }
