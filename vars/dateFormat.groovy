4b8dbb655a63a5b507f0720d10bc228069e00c83
Vladimir Filimonov <69769504+PepegaClasus@users.noreply.github.com>
2022-10-31 06:15:26 -0700

    Update dateFormat.groovy

--- vars/dateFormat.groovy
+++ vars/dateFormat.groovy
@@ -3,7 +3,7 @@ import java.util.*
 import java.text.SimpleDateFormat 
 import java.util.Date
 
-def DateFormat (String dateTime){
+def DateFormat (){
         SimpleDateFormat formatter = new SimpleDateFormat("HH:mm:ss")
         Date date = new Date()
         String newDate = formatter.format(date)
